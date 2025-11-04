772e6de3bbc955d0be33dba8878a146e2d9bec04
Elden <eldendeguia@outlook.com>
2025-11-04 15:10:11 -0600

    Refactored code

--- server.js
+++ server.js
@@ -4,31 +4,58 @@ import pool from "./db.js";
 const app = express();
 
 app.get("/api/question", async (req, res) => {
-    const result = await pool.query("SELECT * FROM questions LIMIT 1;");
-    const { template, template_type, choice_type, table_to_retrieve } =
-        result.rows[0];
-    const answer_query = `SELECT ${template_type}, ${choice_type} FROM ${table_to_retrieve} LIMIT 1;`;
+    const questionObject = await getQuestionObject();
+    const { question, answer } = await formQuestionAndAnswer(questionObject);
+    const wrongAnswers = await getWrongAnswers(questionObject, answer);
 
-    const answerObject = await pool.query(answer_query);
+    res.json({
+        question,
+        answer,
+        wrongAnswers,
+    });
+});
+
+async function getQuestionObject() {
+    const questionQueryResult = await pool.query(
+        "SELECT * FROM questions LIMIT 1;"
+    );
+    return questionQueryResult.rows[0];
+}
+
+async function formQuestionAndAnswer(questionObject) {
+    const { template, template_type, choice_type, table_to_retrieve } =
+        questionObject;
+    const answerQuery = `SELECT ${template_type}, ${choice_type} FROM ${table_to_retrieve} ORDER BY RANDOM() LIMIT 1;`;
+    const answerObject = await pool.query(answerQuery);
     const question = template.replace(
         /<[^>]+>/,
         answerObject.rows[0][template_type]
     );
-    const correctAnswer = answerObject.rows[0][choice_type];
+    const answer = answerObject.rows[0][choice_type];
 
-    const wrong_answer_query = `SELECT ${choice_type} FROM ${table_to_retrieve} WHERE ${choice_type} != '${correctAnswer}' LIMIT 3;`;
-    const wrong_answer_object = await pool.query(wrong_answer_query);
-    const wrong_answers = [];
-    for (const wrong_answer of wrong_answer_object.rows) {
-        wrong_answers.push(wrong_answer[`${choice_type}`]);
+    return { question, answer };
+}
+
+async function getWrongAnswers(questionObject, answer) {
+    const { choice_type, table_to_retrieve } = questionObject;
+    const wrongAnswerQuery = `
+        SELECT ${choice_type}
+        FROM (
+            SELECT DISTINCT ${choice_type}
+            FROM ${table_to_retrieve}
+            WHERE ${choice_type} != '${answer}'
+        )
+        ORDER BY RANDOM()
+        LIMIT 3;
+    `;
+    const wrongAnswerObject = await pool.query(wrongAnswerQuery);
+    const wrongAnswers = [];
+    for (const wrongAnswer of wrongAnswerObject.rows) {
+        wrongAnswers.push(wrongAnswer[`${choice_type}`]);
     }
 
-    res.json({
-        question: question,
-        answer: correctAnswer,
-        wrongAnswers: wrong_answers,
-    });
-});
+    return wrongAnswers;
+}
 
 app.listen(8000, () => {
     console.log("Listening on port 8000");
